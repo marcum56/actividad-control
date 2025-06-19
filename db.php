3601095792cc9a12f681e874cd0827cee70690ab
marcum56 <mcum4735@gmail.com>
2025-06-18 21:55:57 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "dpg-d15o9n15pdvs73drcde0-a.oregon-postgres.render.com";
+$host = "dpg-d19nl895pdvs739vdtl0-a.oregon-postgres.render.com";
 $user = "actividadesmyv20251_db";
-$password = "WYGwGW7eOkNXULdJ1sL3lTalZrR7K5tw";
-$dbname = "actividadesmyv20251_db_mdqu";
+$password = "hVPZVsh9FI8oGVZx74pTCCXTwt2PtYSW";
+$dbname = "actividadesmyv20251_db_5bi0";
 $port = "5432";
 
-//$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+//$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
 
 if (!$conn) {
